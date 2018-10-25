ae5420c1d3ed7e694e4b875234ac87e9ba4ecb5f
Anna Griffin <anna.griffin@students.olin.edu>
2018-10-25 19:17:28 -0400

    determining color

--- color_detector.py
+++ color_detector.py
@@ -30,31 +30,97 @@ def main():
 
 
         mask_red = cv2.inRange(hsv, lower_red, upper_red)
-        # mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
-        # mask_green = cv2.inRange(hsv, lower_green, upper_green)
-        # mask_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
+        mask_red = cv2.erode(mask_red, None, iterations=2)
+        mask_red = cv2.dilate(mask_red, None, iterations=2)
 
-        contours, _ = cv2.findContours(mask_red, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        # blue mask, binary, reduce noise
+        mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
+        mask_blue = cv2.erode(mask_blue, None, iterations=2)
+        mask_blue = cv2.dilate(mask_blue, None, iterations=2)
 
-        for each in contours:
-            area = cv2.contourArea(each)
-            # frame, contours, all of them, (green), thikness=3
-            if area > 1000:
-                cv2.drawContours(frame, contours, -1, (0, 255, 0), 3)
+        mask_green = cv2.inRange(hsv, lower_green, upper_green)
+        mask_green = cv2.erode(mask_green, None, iterations=2)
+        mask_green = cv2.dilate(mask_green, None, iterations=2)
+
+        mask_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
+        mask_yellow = cv2.erode(mask_yellow, None, iterations=2)
+        mask_yellow = cv2.dilate(mask_yellow, None, iterations=2)
+
+        contours_red = cv2.findContours(mask_red.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        red_center = None
+        red_coordinates = []
+
+        contours_blue = cv2.findContours(mask_blue.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        blue_center = None
+        blue_coordinates = []
+
+        contours_green = cv2.findContours(mask_green.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        green_center = None
+        green_coordinates = []
+
+        contours_yellow = cv2.findContours(mask_yellow.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        yellow_center = None
+        yellow_coordinates = []
+
+        if len(contours_red) > 0:
+
+            c = max(contours_red, key=cv2.contourArea)
+            ((x,y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            red_center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0,255,255), 2)
+            cv2.circle(frame, red_center, 5, (0, 0, 255), -1)
+            red_coordinates.append(red_center[1])
+            print("red")
 
 
 
-        res_red = cv2.bitwise_and(frame,frame, mask=mask_red)
-        # res_blue = cv2.bitwise_and(frame,frame, mask=mask_blue)
+        elif len(contours_blue) > 0:
+            c = max(contours_blue, key=cv2.contourArea)
+            ((x,y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            blue_center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0,255,255), 2)
+            cv2.circle(frame, blue_center, 5, (0, 0, 255), -1)
+            blue_coordinates.append(blue_center[1])
+            print("blue")
+        elif len(contours_green) > 0:
+            c = max(contours_green, key=cv2.contourArea)
+            ((x,y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            green_center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0,255,255), 2)
+            cv2.circle(frame, green_center, 5, (0, 0, 255), -1)
+            green_coordinates.append(green_center[1])
+            print("green")
+        elif len(contours_yellow) > 0:
+            c = max(contours_yellow, key=cv2.contourArea)
+            ((x,y), radius) = cv2.minEnclosingCircle(c)
+            M = cv2.moments(c)
+            red_center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0,255,255), 2)
+            cv2.circle(frame, yellow_center, 5, (0, 0, 255), -1)
+            red_coordinates.append(yellow_center[1])
+            print("yellow")
+        else:
+            print(" ")
+
+
+
+
+
+
+        # res_red = cv2.bitwise_and(frame,frame, mask=mask_red)
+        res_blue = cv2.bitwise_and(frame,frame, mask=mask_blue)
         # res_green = cv2.bitwise_and(frame,frame, mask=mask_green)
         # res_yellow = cv2.bitwise_and(frame,frame, mask=mask_yellow)
 
 
         cv2.imshow('frame', frame)
         # cv2.imshow('mask_red', mask_red)
-        # cv2.imshow('mask_blue', mask_blue)
-        cv2.imshow('res_red', res_red)
-        # cv2.imshow('res_blue', res_blue)
+        cv2.imshow('mask_blue', mask_blue)
+        # cv2.imshow('res_red', res_red)
+        cv2.imshow('res_blue', res_blue)
         # cv2.imshow('res_green', res_green)
         # cv2.imshow('res_yellow', res_yellow)
         if cv2.waitKey(1) & 0xFF == ord('q'):
